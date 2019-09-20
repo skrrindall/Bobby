6597d1c47669a553a88f759e3ee204e5b05393a8
skrrindall <skrrindall10387@gmail.com>
2019-09-20 02:11:19 -0500

    More commands

--- bot.js
+++ bot.js
@@ -1,27 +1,39 @@
+// NOTE: this is literally just a testing bot
+// For those who found this repo and want to use this code. The only thing in  `config.json` is TOKEN and PREFIX
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '$';
+const { PREFIX } = require('./config.json');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', (msg) => {
-  if (msg.content === `${prefix}Bob`) {
+  if (msg.content === `${PREFIX}Bob`) {
     msg.reply('Marley!');
-  } else if(msg.content === `${prefix}gay`) {
+  } else if(msg.content === `${PREFIX}gay`) {
    msg.channel.send('U r gay', {files: ['../Test/download.jpeg']})
-  } else if(msg.content === `${prefix}kick`) {
+  } else if(msg.content === `${PREFIX}kick`) {
     // Check for any mentions 
       // Then kick the member using .kick();
-  } else if(msg.content === `${prefix}ban`) {
+  } else if(msg.content === `${PREFIX}ban`) {
     // Check for any mentions
       // Then ban the member using .ban();
+  } else if(msg.content === `${PREFIX}summon`) {
+    client.guilds.find(guild => guild.name=== 'Bot testing')
+    .channels.find(voiceChannel => voiceChannel.name === 'General').join()
+    .then(connection => console.log('Connected!'))
+    .catch(console.error);
+  } else if(msg.content === `${PREFIX}fuckoff`) {
+   client.guilds.find(guild => guild.name === 'Bot testing')
+      .channels.find(voiceChannel => voiceChannel.name === 'General').leave()
   }
-  
 });
 
 client.on("guildMemberAdd", (member) => {
-    // Do what you want to do when a member joins with the member object         
+    const res = ['welcome ShÎthËÆd' , 'Welcome ÇÜmdümþstêr'  ]
+    client.guilds.find(guild => guild.name === 'Bot testing')
+        .channels.find(channel => channel.name === 'general')
+        .send(res[Math.floor(Math.random()*res.length)]);
 });
 
-client.login('NjI0NDAxOTk5Nzg1NDkyNDkw.XYQdsw.a1oqNWGRWCOzv-GSh5rq7KuWfEg');
+client.login(require('./config.json').TOKEN);
